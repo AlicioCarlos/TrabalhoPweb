863b0a67b443d27e301950a718810a5c685c50b9
CarlosAlicio <carlosalicio.developer@gmail.com>
2016-10-04 18:52:35 -0300

    criação das urls, views e pasta static

--- controleacademico/models.py
+++ controleacademico/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
 class Aluno(models.Model):
-    nome = models.ForeignKey('auth.User')
+    user = models.ForeignKey('auth.User')
+    nome = models.CharField(max_length=100)
     matricula = models.IntegerField()
 
     def __str__(self):
         return self.nome
 
 class Professor(models.Model):
-    nome = models.ForeignKey('auth.User')
+    user = models.ForeignKey('auth.User')
+    nome = models.CharField(max_length=100)
     matricula = models.IntegerField()
 
     def __str__(self):
